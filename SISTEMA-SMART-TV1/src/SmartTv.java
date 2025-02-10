1163f3ec16006682c1a5731e8f5ab636db06a07d
samuelvmm <samuelvitor.bfr@gmail.com>
2025-02-10 10:59:18 -0300

    smartTv

---
+++ SISTEMA-SMART-TV1/src/SmartTv.java
@@ -0,0 +1,37 @@
+public class SmartTv {
+    boolean ligada=false;
+    int canal=1;
+    int volume = 25;
+    
+    public void mudarCanal (int novocanl){
+        canal = novocanl;
+    }
+    public void diminuirCanal (){
+        canal --;
+
+    }
+    public void aumentarCanal (){
+        canal ++;
+
+    }
+    public void aumentarVolume(){
+        System.out.println("Aumentando o volume para: " + volume);
+        volume= volume + 1;
+    }
+        
+    public void diminuirVolume(){
+        System.out.println("Diminuindo o volume para: " + volume);
+
+        volume--;
+    }
+
+    public void ligar (){
+        ligada=true;
+    }
+
+    public void desligar (){
+        ligada = false;
+    }
+
+
+}
--- tipos_variaveis/src/TiposVariaveis.java
+++ tipos_variaveis/src/TiposVariaveis.java
@@ -1,5 +1,36 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //tipos primitivos
+           
+           double salarioMinimo = 2500;
+           short numeroCurto = 1;
+        
+            int numeroNormal = numeroCurto;
+            short numeroCurto2 = (short) numeroNormal;
+
+            int numero = 5;
+
+            numero=10;
+
+            System.out.println (numero);
+
+            final double PI = 3.14;
+
+        //classe string que representa texto na aplicação;
+
+            String meuNome = "samuel vitor";
+          
+
+
+            System.out.println(salarioMinimo);
+            System.out.println(numeroCurto);
+            System.out.println(numeroCurto2);
+            System.out.println(PI);
+            System.out.println(meuNome);
+
+
+
+
+
     }
 }
